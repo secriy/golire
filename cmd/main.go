e7a2773865f1d2e629bae2bd3ad2a18415d6e6f6
Secriy <secriyal@gmail.com>
2021-08-21 15:13:42 +0800

    fix arguments can not read

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
 	"time"
 
 	"github.com/secriy/golire"
 )
 
 var (
+	host    = flag.String("i", "", "Host is in CIDR format, such as '192.168.1.0/24'.")
 	ports   = flag.String("p", "22,3389", "Port range, such as '1-28', '22,53,3389' and '22,49-80'.")
 	timeout = flag.Int("t", 200, "Timeout for one scan task, the default is 200 milliseconds.")
 	number  = flag.Int("n", 300, "The number of goroutines to be created.")
@@ -17,23 +17,22 @@ var (
 
 func main() {
 	flag.Usage = func() {
-		fmt.Fprintf(flag.CommandLine.Output(), "Usage of golire: \n  golire [HOST] [OPTIONS], HOST is in CIDR format, such as '192.168.1.0/24'\n\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage of golire: \n  golire -i [HOST] [OPTIONS...]\n\n")
 		flag.PrintDefaults()
 	}
 	flag.Parse()
 	// host must be specified
-	host := flag.Arg(0)
-	if host == "" {
-		fmt.Println("Error: No host specified")
+	if *host == "" {
+		fmt.Println("Error: Host not specified.")
 		flag.Usage()
-		os.Exit(1)
+		return
 	}
 	// running
 	fmt.Println("golire is running, now detecting host...")
-	tasks, err := golire.NewTasks(host, *ports)
+	tasks, err := golire.NewTasks(*host, *ports)
 	if err != nil {
 		fmt.Println("Error: " + err.Error())
-		os.Exit(1)
+		return
 	}
 	fmt.Println("Surviving host: ")
 	for _, v := range tasks.IPPool {
